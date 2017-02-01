4111e84c8c654494b6db10896c4cfc90350d9cdc
Paul Ward <asmodai@gmail.com>
2017-02-01 14:56:41 +0000

    Interp.h somehow wasn't included.

---
+++ Interp.h
@@ -0,0 +1,42 @@
+/*
+ * Interp.h  --- Some title
+ *
+ * Copyright (c) 2017 Paul Ward <asmodai@gmail.com>
+ *
+ * Author:     Paul Ward <asmodai@gmail.com>
+ * Maintainer: Paul Ward <asmodai@gmail.com>
+ * Created:    Wed,  1 Feb 2017 14:29:34 +0000 (GMT)
+ * Keywords:   
+ * URL:        Not distributed yet.
+ */
+/* {{{ License: */
+/*
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, see <http://www.gnu.org/licenses/>.
+ */
+/* }}} */
+/* {{{ Commentary: */
+/*
+ *
+ */
+/* }}} */
+
+void interpret(SyntaxTree *);
+
+
+/* Interp.h ends here */
+/*
+ * Local Variables: ***
+ * indent-tabs-mode: nil ***
+ * End: ***
+ */
