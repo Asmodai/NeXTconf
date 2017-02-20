77c26be0c419eb77d57f6aef91c363f5f71030ed
Paul Ward <asmodai@gmail.com>
2017-02-20 01:00:05 +0000

    Basic functional testing added.

---
+++ Tests/perform.sh
@@ -0,0 +1,46 @@
+#!/bin/sh
+# -*- Mode: Shell-script -*-
+#
+# perform.sh --- Perform unit tests.
+#
+# Copyright (c) 2017 Paul Ward <asmodai@gmail.com>
+#
+# Author:     Paul Ward <asmodai@gmail.com>
+# Maintainer: Paul Ward <asmodai@gmail.com>
+# Created:    Mon, 20 Feb 2017 00:55:10 +0000 (GMT)
+# Keywords:   
+# URL:        not distributed yet
+#
+# {{{ License:
+#
+# This program is free software: you can redistribute it
+# and/or modify it under the terms of the GNU General Public
+# Licenseas published by the Free Software Foundation,
+# either version 3 of the License, or (at your option) any
+# later version.
+#
+# This program isdistributed in the hope that it will be
+# useful, but WITHOUT ANY  WARRANTY; without even the implied
+# warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+# PURPOSE.  See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public
+# License along with this program.  If not, see
+# <http://www.gnu.org/licenses/>.
+#
+# }}}
+# {{{ Commentary:
+#
+# }}}
+
+for test in `ls Test-*`;
+do
+    ../nextconf -f ${test}
+done
+
+# perform.sh ends here
+# Local Variables: ***
+# indent-tabs-mode: nil ***
+# End: ***
+
